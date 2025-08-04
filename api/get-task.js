0d334d129677adae456e17436789b05d84aa3484
ShoorEkko <131442329+ShoorEkko@users.noreply.github.com>
2025-08-04 18:39:04 +0800

    Revert task id

--- api/get-task.js
+++ api/get-task.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
     }
 
     return res.status(200).json({
-        task_id,
+        taskId,
         prompt: promptText,
         filename,
         video_url,
